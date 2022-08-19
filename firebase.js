19c7a12759cd5c5942955d631dcfe72c22c996bc
ErnestCodes <emmycontacts@gmail.com>
2022-08-19 15:17:57 +0100

    deploy version1.0

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC4gl8OsLh5Gfi1MTOkSkT-ORyS--4EnLY",
+  authDomain: "help-fd14d.firebaseapp.com",
+  projectId: "help-fd14d",
+  storageBucket: "help-fd14d.appspot.com",
+  messagingSenderId: "458367144333",
+  appId: "1:458367144333:web:476603d92e5a5cd4a6adc4",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,11 @@
-import React, { Fragment } from "react";
-import { Popover, Transition } from "@headlessui/react";
+import React, { Fragment, useState } from "react";
+import { Popover, Transition, Combobox } from "@headlessui/react";
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
-import { ChevronRightIcon } from "@heroicons/react/solid";
+import {
+  ChevronRightIcon,
+  CheckIcon,
+  SelectorIcon,
+} from "@heroicons/react/solid";
 import FAQ from "./FAQ";
 import Support from "./Support";
 
@@ -12,7 +16,34 @@ const navigation = [
   { name: "Partners", href: "#", id: "partners" },
 ];
 
+const people = [
+  { id: 1, name: "Leslie Alexander" },
+  { id: 2, name: "Leslie Alexander" },
+  { id: 3, name: "Leslie Alexander" },
+  { id: 4, name: "Leslie Alexander" },
+  { id: 5, name: "Leslie Alexander" },
+  { id: 6, name: "Leslie Alexander" },
+  { id: 7, name: "Leslie Alexander" },
+  { id: 8, name: "Leslie Alexander" },
+  { id: 9, name: "Leslie Alexander" },
+  { id: 10, name: "Leslie Alexander" },
+  // More users...
+];
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
 export default function Home() {
+  const [query, setQuery] = useState("");
+  const [selectedPerson, setSelectedPerson] = useState();
+
+  const filteredPeople =
+    query === ""
+      ? people
+      : people.filter((person) => {
+          return person.name.toLowerCase().includes(query.toLowerCase());
+        });
   return (
     <>
       <div className="relative bg-[#013f28] overflow-hidden min-h-screen">
@@ -213,7 +244,7 @@ export default function Home() {
 
                       <div className="mt-6">
                         <form action="#" method="POST" className="space-y-6">
-                          <div>
+                          {/* <div>
                             <label htmlFor="name" className="sr-only">
                               Select Organization
                             </label>
@@ -226,7 +257,80 @@ export default function Home() {
                               required
                               className="block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-gray-300 rounded-md"
                             />
-                          </div>
+                          </div> */}
+                          <Combobox
+                            as="div"
+                            value={selectedPerson}
+                            onChange={setSelectedPerson}
+                          >
+                            <Combobox.Label className="block text-sm font-medium text-gray-700">
+                              Assigned to
+                            </Combobox.Label>
+                            <div className="relative mt-1">
+                              <Combobox.Input
+                                placeholder="Select Organization"
+                                className="w-full rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm"
+                                onChange={(event) =>
+                                  setQuery(event.target.value)
+                                }
+                                displayValue={(person) => person?.name}
+                              />
+                              <Combobox.Button className="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none">
+                                <SelectorIcon
+                                  className="h-5 w-5 text-gray-400"
+                                  aria-hidden="true"
+                                />
+                              </Combobox.Button>
+
+                              {filteredPeople.length > 0 && (
+                                <Combobox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                                  {filteredPeople.map((person) => (
+                                    <Combobox.Option
+                                      key={person.id}
+                                      value={person}
+                                      className={({ active }) =>
+                                        classNames(
+                                          "relative cursor-default select-none py-2 pl-3 pr-9",
+                                          active
+                                            ? "bg-indigo-600 text-white"
+                                            : "text-gray-900"
+                                        )
+                                      }
+                                    >
+                                      {({ active, selected }) => (
+                                        <>
+                                          <span
+                                            className={classNames(
+                                              "block truncate",
+                                              selected && "font-semibold"
+                                            )}
+                                          >
+                                            {person.name}
+                                          </span>
+
+                                          {selected && (
+                                            <span
+                                              className={classNames(
+                                                "absolute inset-y-0 right-0 flex items-center pr-4",
+                                                active
+                                                  ? "text-white"
+                                                  : "text-indigo-600"
+                                              )}
+                                            >
+                                              <CheckIcon
+                                                className="h-5 w-5"
+                                                aria-hidden="true"
+                                              />
+                                            </span>
+                                          )}
+                                        </>
+                                      )}
+                                    </Combobox.Option>
+                                  ))}
+                                </Combobox.Options>
+                              )}
+                            </div>
+                          </Combobox>
 
                           <div>
                             <label
